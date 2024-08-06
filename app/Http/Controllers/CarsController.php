c09a872dc0ddfbcf037fef03ca410d4c77bcb211
and m <andriy.myakush@gmail.com>
2024-08-06 18:57:10 +0100

    auth redirects and i am actually done finally definetyly

--- app/Http/Controllers/CarsController.php
+++ app/Http/Controllers/CarsController.php
@@ -15,6 +15,12 @@ use App\Http\Requests\EditValidationRequest;
 
 class CarsController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth', ['index', 'show']);
+    }
+
     /**
      * Display a listing of the resource.
      */
